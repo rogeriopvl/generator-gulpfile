589fc89858ed017c7ae3a574cabbdbd574050c66
rogeriopvl <rogeriopvl@gmail.com>
2014-01-07 14:12:13 +0000

    Add test cases

--- test/test-creation.js
+++ test/test-creation.js
@@ -19,19 +19,62 @@ describe('gulpfile generator', function () {
         }.bind(this));
     });
 
-    it('creates expected files', function (done) {
-        var expected = [
-            // add files you expect to exist here.
-            '.jshintrc',
-            '.editorconfig'
-        ];
+    it('creates expected browser app files with package.json', function (done) {
+        var expected = [ 'Gulpfile.js', 'package.json' ];
 
         helpers.mockPrompt(this.app, {
-            'someOption': true
+            'createPkgFile': true,
+            'appType': 'browser'
         });
+
         this.app.options['skip-install'] = true;
         this.app.run({}, function () {
-            helpers.assertFiles(expected);
+            helpers.assertFiles(expected, /gulp-livereload/);
+            done();
+        });
+    });
+
+    it('creates expected node app files with package.json', function (done) {
+        var expected = [ 'Gulpfile.js', 'package.json' ];
+
+        helpers.mockPrompt(this.app, {
+            'createPkgFile': true,
+            'appType': 'browser'
+        });
+
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+            helpers.assertFiles(expected, /gulp-mocha/);
+            done();
+        });
+    });
+
+    it('creates expected node app files without package.json', function (done) {
+        var expected = [ 'Gulpfile.js' ];
+
+        helpers.mockPrompt(this.app, {
+            'createPkgFile': false,
+            'appType': 'node'
+        });
+
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+            helpers.assertFiles(expected, /gulp-mocha/);
+            done();
+        });
+    });
+
+    it('creates expected browser app files without package.json', function (done) {
+        var expected = [ 'Gulpfile.js' ];
+
+        helpers.mockPrompt(this.app, {
+            'createPkgFile': false,
+            'appType': 'browser'
+        });
+
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+            helpers.assertFiles(expected, /gulp-livereload/);
             done();
         });
     });
